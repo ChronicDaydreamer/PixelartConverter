b65596e9cf1b7b3d61d029d3059dd0b52547564b
Micah Thomas <65049602+ChronicDaydreamer@users.noreply.github.com>
2026-03-09 19:25:09 -0700

    added toGreyscale() and built up toBlackAndWhite()

--- Pixelizer.java
+++ Pixelizer.java
@@ -12,7 +12,7 @@ public class Pixelizer {
 
     }
 
-    public BufferedImage imageReader(String path){
+    public void imageReader(String path){
         try {
             File inputFile = new File(path);
             BufferedImage image = ImageIO.read(inputFile);
@@ -20,10 +20,9 @@ public class Pixelizer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return this.ogImage;
     }
 
-    public BufferedImage reduceResolution(int stepSize){
+    public void reduceResolution(int stepSize){
         BufferedImage newImage = new BufferedImage(this.ogImage.getWidth()/stepSize, this.ogImage.getHeight()/stepSize, BufferedImage.TYPE_INT_RGB);
         for(int y=stepSize-1;y<this.ogImage.getHeight();y+=stepSize){
             for(int x=stepSize-1;x<this.ogImage.getWidth();x+=stepSize){ 
@@ -48,27 +47,70 @@ public class Pixelizer {
                 newImage.setRGB(x/(stepSize), y/(stepSize), rgbValue);
             }
         }  
-        return newImage;
+        this.ogImage=newImage;
     }
 
-    public void imageToPNG(BufferedImage image){
+    public void imageToPNG(){
         try{
             File outputFile = new File("output.png");
-            ImageIO.write(image,"PNG",outputFile);
+            ImageIO.write(this.ogImage,"PNG",outputFile);
 
         }catch(IOException e){
             System.out.println("error saving to file");
         }
     }
 
-    public BufferedImage toBlackAndWhite(BufferedImage image){
-        return image;
+    public void toGreyscale(){
+        for(int y=0;y<this.ogImage.getHeight();y+=1){
+            for(int x=0;x<this.ogImage.getWidth();x+=1){ 
+                int pixelValue = this.ogImage.getRGB(x, y);
+                int a=(pixelValue >> 24) & 0xFF;
+                int r=(pixelValue >> 16) & 0xFF;
+                int g=(pixelValue >> 8) & 0xFF;
+                int b=pixelValue & 0xFF;
+
+                int averageRGB=(r+g+b)/3;
+                a=averageRGB;
+                r=averageRGB;
+                g=averageRGB;
+                b=averageRGB;
+                int rgbValue = (a << 24) | (r << 16) | (g << 8) | b;
+                this.ogImage.setRGB(x, y, rgbValue);
+            }
+        }
     }
-    
+    public void toBlackAndWhite(double threshold){
+        for(int y=0;y<this.ogImage.getHeight();y+=1){
+            for(int x=0;x<this.ogImage.getWidth();x+=1){ 
+                int pixelValue = this.ogImage.getRGB(x, y);
+                int a=(pixelValue >> 24) & 0xFF;
+                int r=(pixelValue >> 16) & 0xFF;
+                int g=(pixelValue >> 8) & 0xFF;
+                int b=pixelValue & 0xFF;
+                int averageRGB=(r+g+b)/3;
+                if(averageRGB>threshold*255){
+                    a=255;
+                    r=255;
+                    g=255;
+                    b=255;
+                }else{
+                    a=255;
+                    r=0;
+                    g=0;
+                    b=0; 
+                }
+                
+                int rgbValue = (a << 24) | (r << 16) | (g << 8) | b;
+                this.ogImage.setRGB(x, y, rgbValue);
+            }
+        }
+    }
     public static void main(String[] args){
         Pixelizer pixelizer=new Pixelizer();
         pixelizer.imageReader("testImage.jpg");
-        BufferedImage image =pixelizer.reduceResolution(Integer.parseInt(args[0]));
-        pixelizer.imageToPNG(image);
+        //BufferedImage image =pixelizer.reduceResolution(Integer.parseInt(args[0]));
+        //pixelizer.toGreyscale();
+        pixelizer.toBlackAndWhite(Double.parseDouble(args[1]));
+        pixelizer.imageToPNG();
     }
 }
